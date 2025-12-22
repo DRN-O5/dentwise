d141fdda8ebc64fe4e753a7e9cdf2f1ad88b235c
DRN-O5 <dibyaranjan1805@gmail.com>
2025-12-22 16:09:54 +0530

    added landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,26 @@ import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignOutButton, SignUpButton } from "@clerk/nextjs";
 import Image from "next/image";
 
+import Header from "@/components/landing/Header";
+import Hero from "@/components/landing/Hero";
+import HowItWorks from "@/components/landing/HowItWorks";
+import WhatToAsk from "@/components/landing/WhatToAsk";
+import PricingSection from "@/components/landing/PricingSection";
+import CTA from "@/components/landing/CTA";
+import Footer from "@/components/landing/Footer";
+
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <SignedOut>
-        <SignUpButton mode="modal">Sign Up</SignUpButton>
-      </SignedOut>
+    <div className="min-h-screen bg-background">
+      <Header />
+      <Hero />
+      <HowItWorks />
+      <WhatToAsk />
+      <PricingSection />
+      <CTA />
+      <Footer />
 
-      <SignedIn>
-        <SignOutButton>Log Out</SignOutButton>
-      </SignedIn>
     </div>
   );
 }
